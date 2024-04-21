542bcfd8d0bd2c09ed2e1dad278ad06d6267811b
Yazdan Musa <yazdanmusa@gmail.com>
2024-04-21 14:43:42 +0200

    Add Bootstrap styling to the app layout and update chart widths.
    - Integrate Bootstrap styles for layout components
    - Adjust chart widths to 100% for responsiveness

--- app.py
+++ app.py
@@ -1,52 +1,86 @@
 from dash import Dash, html, dcc, Input, Output, callback, ctx
+import dash_bootstrap_components as dbc
 from datetime import datetime, date, timedelta
 import os
 from charts import create_elec_prod_bar_chart, create_elec_prod_line_chart, create_elec_prod_pie_chart
 
-app = Dash(__name__)
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 app.title = 'Energy Production in Switzerland'
 
 today = date.today()
 seven_days_ago = today - timedelta(days=7)
 
-def serve_layout():
-    return html.Div([
-        dcc.Location(id='url', refresh=False, href='/bar'),
-        html.Div([
-            html.H1('Energy Production in Switzerland', style={'textAlign': 'center'}),
-            dcc.Link('Line Chart', href='/elec-prod-line'),
-            html.Br(),
-            dcc.Link('Bar Chart', href='/elec-prod-bar'),
-            html.Br(),
-            dcc.Link('Pie Chart', href='/elec-prod-pie'),
-        ]),
-        html.Div([
-            html.Label('Select Start Date:'),
-            dcc.DatePickerSingle(
-                id='start-date-picker',
-                min_date_allowed=datetime(2020, 1, 1),
-                max_date_allowed=datetime.today(),
-                initial_visible_month=datetime.today(),
-                date=seven_days_ago,
-                display_format='DD.MM.YYYY'
-            ),
-            html.Label('Select End Date:'),
-            dcc.DatePickerSingle(
-                id='end-date-picker',
-                min_date_allowed=datetime(2020, 1, 1),
-                max_date_allowed=datetime.today(),
-                initial_visible_month=datetime.today(),
-                date=today,
-                display_format='DD.MM.YYYY'
-            )
-        ], style={'padding': 10}),
-        html.Div(id='page-content')
-    ])
+SIDEBAR_STYLE = {
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "250px",
+    "padding": "1rem 1rem",
+    "background-color": "#f8f9fa",
+}
 
-app.layout = serve_layout
+CONTENT_STYLE = {
+    "flex-grow": 1,
+    "padding": "1rem 1rem",
+    "width": "100%"
+}
 
-@app.callback(Output('page-content', 'children'),
+MAIN_LAYOUT_STYLE = {
+    "display": "flex",
+    "flex-direction": "row",
+    "height": "100vh",
+}
+
+sidebar = html.Div(
+    [
+        html.P(
+            "Electricity Production in Switzerland", className="h3"
+        ),
+        dbc.Nav(
+            [
+                dbc.NavLink("Line Chart", href="/elec-prod-line", active="exact"),
+                dbc.NavLink("Bar Chart", href="/elec-prod-bar", active="exact"),
+                dbc.NavLink("Pie Chart", href="/elec-prod-pie", active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+date_pickers = html.Div([
+    html.Label('Start Date:'),
+    dcc.DatePickerSingle(
+        id='start-date-picker',
+        min_date_allowed=datetime(2020, 1, 1),
+        max_date_allowed=datetime.today(),
+        initial_visible_month=datetime.today(),
+        date=seven_days_ago,
+        display_format='DD.MM.YYYY'
+    ),
+    html.Label('End Date:'),
+    dcc.DatePickerSingle(
+        id='end-date-picker',
+        min_date_allowed=datetime(2020, 1, 1),
+        max_date_allowed=datetime.today(),
+        initial_visible_month=datetime.today(),
+        date=today,
+        display_format='DD.MM.YYYY'
+    )
+], style={'display': 'flex', 'align-items':'center', 'padding': '5px', 'margin-bottom': '15px'})
+
+content = html.Div(
+    [
+        date_pickers,
+        html.Div(id="page-content", style=CONTENT_STYLE)
+    ],
+    style=CONTENT_STYLE
+)
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content], style=MAIN_LAYOUT_STYLE)
+
+@app.callback(Output("page-content", "children"),
               [Input('url', 'pathname'), Input('start-date-picker', 'date'), Input('end-date-picker', 'date')])
 def display_page(pathname, start_date, end_date):
     if pathname == '/elec-prod-line':
--- charts.py
+++ charts.py
@@ -7,7 +7,7 @@ def create_elec_prod_bar_chart(start_date, end_date):
     fig = px.bar(df, x='Time', y='Power (MW)', color='Production Type',
                  labels={'Time': 'Time of Day', 'Power (MW)': 'Power in MW'},
                  title='Bar Chart: Energy Production Types Over Time')
-    return dcc.Graph(figure=fig)
+    return dcc.Graph(figure=fig,  style={'width': '100%'})
 
 def create_elec_prod_line_chart(start_date, end_date):
     df = get_public_power(start_date, end_date)
@@ -15,7 +15,7 @@ def create_elec_prod_line_chart(start_date, end_date):
                   labels={'Time': 'Time of Day', 'Power (MW)': 'Power in MW'},
                   title='Line Chart: Energy Production Types Over Time')
     fig.update_xaxes(rangeslider_visible=True)
-    return dcc.Graph(figure=fig)
+    return dcc.Graph(figure=fig,  style={'width': '100%'})
 
 def create_elec_prod_pie_chart(start_date, end_date):
     df = get_public_power(start_date, end_date)
@@ -24,4 +24,4 @@ def create_elec_prod_pie_chart(start_date, end_date):
     fig = px.pie(df_sum, values='Power (MW)', names='Production Type',
                  title='Pie Chart: Total Energy Production by Type',
                  labels={'Production Type': 'Production Type', 'Power (MW)': 'Total Power in MW'})
-    return dcc.Graph(figure=fig)
+    return dcc.Graph(figure=fig,  style={'width': '100%'})
