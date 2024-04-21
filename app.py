a470b97e6f90b97f0feecee1ca36b5d2c2f082ab
Yazdan Musa <yazdanmusa@gmail.com>
2024-04-21 13:56:37 +0200

    Refactor chart creation and routing logic, add pie chart
    - Added functions to create bar, line, and pie charts for energy production
    - Updated routing to use new chart creation functions

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from dash import Dash, html, dcc, Input, Output, callback, ctx
-import plotly.express as px
-from api.public_power import get_public_power
-import os
 from datetime import datetime, date, timedelta
+import os
+from charts import create_elec_prod_bar_chart, create_elec_prod_line_chart, create_elec_prod_pie_chart
 
 app = Dash(__name__)
 server = app.server
-
 app.title = 'Energy Production in Switzerland'
 
 today = date.today()
@@ -14,12 +12,14 @@ seven_days_ago = today - timedelta(days=7)
 
 def serve_layout():
     return html.Div([
-        dcc.Location(id='url', refresh=False),
+        dcc.Location(id='url', refresh=False, href='/bar'),
         html.Div([
             html.H1('Energy Production in Switzerland', style={'textAlign': 'center'}),
-            dcc.Link('Line Chart', href='/line'),
+            dcc.Link('Line Chart', href='/elec-prod-line'),
             html.Br(),
-            dcc.Link('Bar Chart', href='/bar'),
+            dcc.Link('Bar Chart', href='/elec-prod-bar'),
+            html.Br(),
+            dcc.Link('Pie Chart', href='/elec-prod-pie'),
         ]),
         html.Div([
             html.Label('Select Start Date:'),
@@ -28,7 +28,7 @@ def serve_layout():
                 min_date_allowed=datetime(2020, 1, 1),
                 max_date_allowed=datetime.today(),
                 initial_visible_month=datetime.today(),
-                date = seven_days_ago,
+                date=seven_days_ago,
                 display_format='DD.MM.YYYY'
             ),
             html.Label('Select End Date:'),
@@ -37,7 +37,7 @@ def serve_layout():
                 min_date_allowed=datetime(2020, 1, 1),
                 max_date_allowed=datetime.today(),
                 initial_visible_month=datetime.today(),
-                date = today,
+                date=today,
                 display_format='DD.MM.YYYY'
             )
         ], style={'padding': 10}),
@@ -49,28 +49,13 @@ app.layout = serve_layout
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname'), Input('start-date-picker', 'date'), Input('end-date-picker', 'date')])
 def display_page(pathname, start_date, end_date):
-    if pathname == '/line':
-        return line_chart_layout(start_date, end_date)
-    elif pathname == '/bar':
-        return bar_chart_layout(start_date, end_date)
+    if pathname == '/elec-prod-line':
+        return create_elec_prod_line_chart(start_date, end_date)
+    elif pathname == '/elec-prod-bar':
+        return create_elec_prod_bar_chart(start_date, end_date)
+    elif pathname == '/elec-prod-pie':
+        return create_elec_prod_pie_chart(start_date, end_date)
     else:
-        return "Welcome! Please select a chart type from the menu."
-
-def line_chart_layout(start_date, end_date):
-    df = get_public_power(start_date, end_date)
-    fig = px.line(df, x='Time', y='Power (MW)', color='Production Type',
-                  labels={'Time': 'Time of Day', 'Power (MW)': 'Power in MW'},
-                  title='Line Chart: Energy Production Types Over Time')
-    fig.update_xaxes(rangeslider_visible=True)
-    return dcc.Graph(figure=fig)
-
-# Define the bar chart layout
-def bar_chart_layout(start_date, end_date):
-    df = get_public_power(start_date, end_date)
-    fig = px.bar(df, x='Time', y='Power (MW)', color='Production Type',
-                 labels={'Time': 'Time of Day', 'Power (MW)': 'Power in MW'},
-                 title='Bar Chart: Energy Production Types Over Time')
-    return dcc.Graph(figure=fig)
-
+        return create_elec_prod_bar_chart(start_date, end_date)
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', port=int(os.environ.get('PORT', 8050)))
---
+++ charts.py
@@ -0,0 +1,27 @@
+import plotly.express as px
+from dash import dcc
+from data import get_public_power
+
+def create_elec_prod_bar_chart(start_date, end_date):
+    df = get_public_power(start_date, end_date)
+    fig = px.bar(df, x='Time', y='Power (MW)', color='Production Type',
+                 labels={'Time': 'Time of Day', 'Power (MW)': 'Power in MW'},
+                 title='Bar Chart: Energy Production Types Over Time')
+    return dcc.Graph(figure=fig)
+
+def create_elec_prod_line_chart(start_date, end_date):
+    df = get_public_power(start_date, end_date)
+    fig = px.line(df, x='Time', y='Power (MW)', color='Production Type',
+                  labels={'Time': 'Time of Day', 'Power (MW)': 'Power in MW'},
+                  title='Line Chart: Energy Production Types Over Time')
+    fig.update_xaxes(rangeslider_visible=True)
+    return dcc.Graph(figure=fig)
+
+def create_elec_prod_pie_chart(start_date, end_date):
+    df = get_public_power(start_date, end_date)
+    df = df[df['Production Type'] != 'Load']
+    df_sum = df.groupby('Production Type')['Power (MW)'].sum().reset_index()
+    fig = px.pie(df_sum, values='Power (MW)', names='Production Type',
+                 title='Pie Chart: Total Energy Production by Type',
+                 labels={'Production Type': 'Production Type', 'Power (MW)': 'Total Power in MW'})
+    return dcc.Graph(figure=fig)
